71578a8506792c0def5955bcc210641de6af9266
DongHun Kim <31065684+d-h-k@users.noreply.github.com>
2019-07-30 15:51:36 +0900

    Add files via upload

---
+++ dhkimsetup.sh
@@ -0,0 +1,12 @@
+sudo apt-get install update;
+sudo apt-get install -y upgrade;
+sudo apt-get install -y bulid-essential;
+sudo apt-get install -y vim;
+sudo apt-get install -y git;
+sudo apt-get install -y cmake;
+sudo apt-get install -y python;
+sudo apt-get install -y python3;
+sudo apt-get install -y python-pip;
+sudo apt-get install -y python3-pip;
+sudo apt-get install -y minicom
+sudo apt-get install -y terminator
