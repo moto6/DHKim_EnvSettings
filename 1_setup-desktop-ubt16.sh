dbe3df193b4a5185471dfbe5b12def1cba06a2cf
DongHun Kim <31065684+d-h-k@users.noreply.github.com>
2019-08-27 10:13:45 +0900

    Update 1_setup-desktop-ubt16.sh

--- 1_setup-desktop-ubt16.sh
+++ 1_setup-desktop-ubt16.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
 
-sudo apt-get install -y apt-utils cmake unzip gedit vim git pkg-config build-essential cmake libjpeg-dev libgflags-dev libtiff5-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libxvidcore-dev libx264-dev libxine2-dev libv4l-dev v4l-utils python2.7-dev python3-dev python-numpy python3-numpy python-pip python3-pip terminator
+sudo apt-get install -y apt-utils cmake unzip gedit vim git pkg-config build-essential cmake libjpeg-dev libgflags-dev libtiff5-dev libpng-dev libavcodec-dev libavformat-dev libswscale-dev libxvidcore-dev libx264-dev libxine2-dev libv4l-dev v4l-utils python2.7-dev python3-dev python-numpy python3-numpy python-pip python3-pip
 
 #sudo apt-get install -y minicom
 #pip3 install numpy scipy matplotlib ipython scikit-learn pandas
